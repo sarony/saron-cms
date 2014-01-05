eedda149d4e2b47014fe2da362eb5ce4f01a5b0e
sarony <saron.yitbarek@gmail.com>
2014-01-05 17:45:53 -0500

    delete button works. edit and show are buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ SaronCms::Application.routes.draw do
   get '/posts' => "posts#index", :as => "posts"
   get '/posts/new' => "posts#new", :as => "new_post"
   post '/posts' => "posts#create"
+  delete '/posts/:id' => "posts#destroy"
   get '/posts/:id' => "posts#show", :as => "post"
   get '/posts/:id/edit' => "posts#edit", :as => "edit_post"
   patch '/posts/:id' => "posts#update"
-  delete '/posts/:id' => "posts#destroy"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
